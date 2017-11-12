e4d1f124b2a836bbffa7ad26822813666d3dc1fa
saul <slbrtg@gmail.com>
2017-11-11 18:48:58 -0800

    Add firebase dependecies to android

--- app/main.ts
+++ app/main.ts
@@ -3,4 +3,6 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
 
+
+
 platformNativeScriptDynamic().bootstrapModule(AppModule);
